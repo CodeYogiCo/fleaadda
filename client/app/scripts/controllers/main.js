f7137879d950ff70b76bbc21e91fbd342c2f494f
Vaseem Mohammed <vaseem.mohammed@ugamsolutions.com>
2014-12-09 22:49:18 +0530

    project with angular created.

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fleaaddaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fleaaddaApp
+ */
+angular.module('fleaaddaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
